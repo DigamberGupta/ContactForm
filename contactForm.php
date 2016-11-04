bb07ebc14fb86c168f983aba6a204af48e211b72
DigamberGupta <digambergupta18@gmail.com>
2016-11-04 20:40:00 +0530

    Validation to contact form added
    validation is added to the form with the help of Jquery
    and in the contact form we check email, subject , and  content of the
    client

--- contactForm.php
+++ contactForm.php
@@ -18,10 +18,12 @@
   <body>
     
 
-		    <form>
-	 <div class="container">
+<form>
+		 <div class="container">
 
 			  <h1>Get in Touch</h1>
+			  <div class="error" ></div>
+
 		  <div class="form-group">
 		    <label for="exampleInputEmail1">Email address</label>
 		    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter youe email address">
@@ -37,15 +39,54 @@
 		    <textarea class="form-control" id="content" rows="3"></textarea>
 		  </div>
 
-		  
-		  <button type="submit" class="btn btn-primary">Submit</button>
+		  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
 		</form>
 
 	 </div>
 
+	 	
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+
+	 		 $("form").submit(function(e){
+       				 e.preventDefault();
+
+       				var error="";
+
+       				if($("#email").val() ==""){
+
+			 			error +="The email is empty<br>";
+			 		}
+
+			 		if($("#subject").val() ==""){
+
+			 			error +="The subject is empty<br>";
+			 		}
+			 		 if($("#content").val() ==""){
+
+	 		 			error +="The content is empty";
+
+	 				 }
+
+			 		if(error !=""){
+			 			$(".error").html('<div class="alert alert-danger" role="alert"><strong>There were some errors in this form :<br></strong> '+ error +'</div>' );
+			 		}else{
+			 			 $("form").unbind("submit").submit();
+			 		}
+
+
+   				 });
+	 			
+
+	 		
+
+	 	</script>
+
+
+
   </body>
 </html>
